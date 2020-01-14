89efa8f04db061a4923f03240d84354e6ad00e8d
maxim122 <41864450+maxim122@users.noreply.github.com>
2020-01-14 19:48:53 +0300

    Update header.php

--- header.php
+++ header.php
@@ -7,16 +7,7 @@
         <meta name="theme-color" content="#c9e0e04d">
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <link rel="shortcut icon" href="<?php echo bloginfo("template_url"); ?>/assets/img/favicons/favicon.ico" type="image/x-icon">
-        <link rel="apple-touch-icon" href="<?php echo bloginfo("template_url"); ?>/assets/img/favicons/apple-touch-icon.png">
-        <link rel="apple-touch-icon" sizes="57x57" href="<?php echo bloginfo("template_url"); ?>/assets/img/favicons/apple-touch-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="<?php echo bloginfo("template_url"); ?>/assets/img/favicons/apple-touch-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="<?php echo bloginfo("template_url"); ?>/assets/img/favicons/apple-touch-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="<?php echo bloginfo("template_url"); ?>/assets/img/favicons/apple-touch-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="<?php echo bloginfo("template_url"); ?>/assets/img/favicons/apple-touch-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="<?php echo bloginfo("template_url"); ?>/assets/img/favicons/apple-touch-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo bloginfo("template_url"); ?>/assets/img/favicons/apple-touch-icon-180x180.png">
-        <link rel="stylesheet" href="./styles/main.min.css">
+        
         <?php wp_head();?>
     </head>
     <body class="no-svg">
@@ -67,23 +58,7 @@
                             'items_wrap'      => '<ul class="header__nav">%3$s</ul>',
                             'depth'           => 1
                         ] );?>
-                    <!-- <ul class="header__nav">
-                        <li>
-                            <a class="header__nav-item" href="#about">Про компанию</a>
-                        </li>
-                        <li>
-                            <a class="header__nav-item" href="#specialists">Наша команда</a>
-                        </li>
-                        <li>
-                            <a class="header__nav-item" href="#toys">Игрушки</a>
-                        </li>
-                        <li>
-                            <a class="header__nav-item" href="#aboutus">Наша история</a>
-                        </li>
-                        <li>
-                            <a class="header__nav-item" href="#contacts">Контакты и отзывы</a>
-                        </li>
-                    </ul> -->
+                    
                 </nav>
             </div>
             <div class="header__hamburger">
